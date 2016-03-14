57cb0665fbc823ef922dcdf6e3a9df7e1c55c7f3
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-14 14:21:31 -0500

    Assignment Commit lecture-09

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var shoe = 20;
-var soul = "dirt"
-var  = true;
+var shoe;
+var socks;
+var shirt;
+var pants;
+var hat;
+
+var bike = 2;
+var wheel = 4;
+var skateboard = 6;
+var tv = 8;
+var internet = 10;
+
+
+
+var bike = "hello";
+var wheel = 4 "goodbye";
+var skateboard = 6 "greetings";
+var tv = 8 "partings";
+var internet = 10 "reunion";
 
 
 
 
-if
 
 var game = "basketball";
 
@@ -27,5 +42,30 @@ switch (game) {
   console.log("On the bench")
 }
 
-var = denied = false, age 14;
-age >= 16 denied = true;
+var bike = true;
+var wheel = false;
+var skateboard = true;
+var tv = false;
+var internet = true;
+
+
+
+// var denied = false, age = 14;
+// age >= 16 denied = true;
+
+
+
+var denied = true;
+var age = 14;
+console.log ("true")
+
+if (age >= 16) {
+  denied = false;
+  console.log ("false")
+}
+
+
+var a = 3;
+var b = 2;
+
+var total = a + b;
