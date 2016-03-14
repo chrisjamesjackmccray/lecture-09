4c73f45e10e761cd10d515d4867cbb794087ad4c
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-14 13:18:22 -0500

    Assignment Commit lecture-09

---
+++ index.js
@@ -0,0 +1,23 @@
+var shoe = 42;
+var shoe = "boot"
+var shoe = true;
+
+var game = "basketball";
+
+switch (game) {
+  case "basketball":
+
+    break;
+
+  case "soccer";
+
+  break;
+
+  case "football";
+
+  break;
+
+  default:
+
+  console.log("On the bench")
+}
